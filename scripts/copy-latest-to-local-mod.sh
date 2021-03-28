b69c09f4c61ebbfe156605a0d7f395c1c6fa6250
Immo Sanders-Sjuts <immosanders-sjuts@web.de>
2021-03-28 09:47:14 +0200

    Added feedback to bash scripts

--- scripts/copy-latest-to-local-mod.sh
+++ scripts/copy-latest-to-local-mod.sh
@@ -2,3 +2,5 @@
 
 # Copy the contents of the last modified folder in ../mod/ to the default location for local Stellaris mods
 cp -TR mod/$(ls -tr $(dirname $0)/../mod/ | tail -1) ~/Documents/Paradox\ Interactive/Stellaris/mod/$(ls -tr $(dirname $0)/../mod/ | tail -1)
+
+echo "$(ls -tr $(dirname $0)/../mod/ | tail -1)/ copied to \"~/Documents/Paradox Interactive/Stellaris/mod/\""
--- scripts/overwrite-local-mod-with-latest.sh
+++ scripts/overwrite-local-mod-with-latest.sh
@@ -3,5 +3,9 @@
 # Remove recursively the content of the target folder with the same name as the one that was last modified in ../mod/
 rm -rf ~/Documents/Paradox\ Interactive/Stellaris/mod/$(ls -tr $(dirname $0)/../mod/ | tail -1)
 
+echo cleaned \"~/Documents/Paradox\ Interactive/Stellaris/mod/$(ls -tr $(dirname $0)/../mod/ | tail -1)/\"
+
 # Copy the contents of the last modified folder in ../mod/ to the default location for local Stellaris mods
 cp -TR mod/$(ls -tr $(dirname $0)/../mod/ | tail -1) ~/Documents/Paradox\ Interactive/Stellaris/mod/$(ls -tr $(dirname $0)/../mod/ | tail -1)
+
+echo "$(ls -tr $(dirname $0)/../mod/ | tail -1)/ copied to \"~/Documents/Paradox Interactive/Stellaris/mod/\""
